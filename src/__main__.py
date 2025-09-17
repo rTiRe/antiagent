64497d7b3928cfe6434894e700b9cbcefa7e0fd4
TiRe <zientenin@mail.ru>
2025-09-17 22:55:02 +0300

    feat: edit message handler

--- src/__main__.py
+++ src/__main__.py
@@ -9,13 +9,35 @@ load_dotenv()
 
 TOKEN = getenv('TOKEN')
 AGENTS_IDS = list(map(int, getenv('AGENT_IDS').strip().split(',')))
-DISCLAIMER = 'НАСТОЯЩИЙ МАТЕРИАЛ (ИНФОРМАЦИЯ) ПРОИЗВЕДЕН, РАСПРОСТРАНЕН И (ИЛИ) НАПРАВЛЕН ИНОСТРАННЫМ АГЕНТОМ АЛЕКСЕЕМ НЕГРОВИЧЕМ ЗАЙЦЕВЫМ ЛИБО КАСАЕТСЯ ДЕЯТЕЛЬНОСТИ ИНОСТРАННОГО АГЕНТА АЛЕКСЕЯ НЕГРОВИЧА ЗАЙЦЕВА'
+DISCLAIMERS = [
+    'НАСТОЯЩИЙ МАТЕРИАЛ (ИНФОРМАЦИЯ) ПРОИЗВЕДЕН, РАСПРОСТРАНЕН И (ИЛИ) НАПРАВЛЕН ИНОСТРАННЫМ АГЕНТОМ АЛЕКСЕЕМ НЕГРОВИЧЕМ ЗАЙЦЕВЫМ ЛИБО КАСАЕТСЯ ДЕЯТЕЛЬНОСТИ ИНОСТРАННОГО АГЕНТА АЛЕКСЕЯ НЕГРОВИЧА ЗАЙЦЕВА',
+]
+
+async def check_is_agent(telegram_id: int) -> bool:
+    return telegram_id in AGENTS_IDS
+
+
+async def check_has_disclaimer(text: str = '') -> bool:
+    if not isinstance(text, str):
+        return False
+    text = text.upper()
+    has_disclaimer = False
+    for disclaimer in DISCLAIMERS:
+        has_disclaimer = text.startswith(disclaimer)
+        if has_disclaimer:
+            break
+    return has_disclaimer
+
+
+async def check_needs_delete(message: Message) -> bool:
+    is_agent = await check_is_agent(message.from_user.id)
+    has_disclaimer = await check_has_disclaimer(message.text or '')
+    return is_agent and not has_disclaimer
+
 
 async def delete_agent_message(message: Message) -> None:
-    is_agent = message.from_user.id in AGENTS_IDS
-    has_disclaimer = message.text.startswith(DISCLAIMER)
-    if is_agent and not has_disclaimer:
-        await message.answer(f'@{message.from_user.username} Вам запрещено отправлять сообщения без дисклеймера о том, что Вы являетесь иностранным агентом.')
+    if await check_needs_delete(message):
+        await message.answer(f'@{message.from_user.username}, Вам запрещено отправлять сообщения без дисклеймера о том, что Вы являетесь иностранным агентом.')
         await message.delete()
 
 
@@ -23,6 +45,7 @@ async def main() -> None:
     dispatcher = Dispatcher()
     bot = Bot(token=TOKEN)
     dispatcher.message.register(delete_agent_message)
+    dispatcher.edited_message.register(delete_agent_message)
     await dispatcher.start_polling(bot)
 
 
